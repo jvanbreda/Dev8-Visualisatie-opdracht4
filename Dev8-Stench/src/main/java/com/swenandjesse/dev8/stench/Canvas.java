d0e1348f3faf50f60ac9d72eb1b5004212457de8
jvanbreda <jvbreda1995@gmail.com>
2016-07-01 00:07:07 +0200

    Added: threads to speed up the process a little + little tweaks for the google geocode API implementation

--- Dev8-Stench/src/main/java/com/swenandjesse/dev8/stench/Canvas.java
+++ Dev8-Stench/src/main/java/com/swenandjesse/dev8/stench/Canvas.java
@@ -6,11 +6,15 @@
 package com.swenandjesse.dev8.stench;
 
 import com.swenandjesse.dev8.stench.data.DataProvider;
+import com.swenandjesse.dev8.stench.models.Complaint;
+import com.swenandjesse.dev8.stench.models.ComplaintCoordinates;
 import com.swenandjesse.dev8.stench.models.Crematoria;
 import com.swenandjesse.dev8.stench.models.Rect;
 import com.swenandjesse.dev8.stench.models.Vector2;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -25,6 +29,12 @@ import processing.event.MouseEvent;
  */
 public class Canvas extends PApplet {
     
+    public List<Complaint> complaints = Collections.synchronizedList(new ArrayList<Complaint>());
+    public List<ComplaintCoordinates> complaintsCoordinates = Collections.synchronizedList(new ArrayList<ComplaintCoordinates>());
+    private DataProvider provider;
+    
+    Canvas canvas;
+
     private final float maxScale = 2f;
     private Vector2<Integer> worldSize;
     private Vector2<Integer> viewport;
@@ -33,18 +43,18 @@ public class Canvas extends PApplet {
     private float scale;
 
     private Vector2<Integer> mousePosition;
-    
+
     private PImage testImage;
-    
-    private List<Crematoria> crematorias;
+
+    public List<Crematoria> crematorias = Collections.synchronizedList(new ArrayList<Crematoria>());
 
     @Override
     public void setup() {
         frame.setTitle("Jesse and Swen - Development 8 - Assignment 4 - Stankoverlast");
-        
+
 //        testImage = loadImage("testimage.jpg");
         testImage = loadImage("map.png");
-        
+
         worldSize = new Vector2<>(testImage.width, testImage.height);
         viewport = new Vector2<>(600, 600);
         position = new Vector2<>(0f, 0f);
@@ -54,12 +64,41 @@ public class Canvas extends PApplet {
         frameRate(60);
 
         mousePosition = new Vector2<>(mouseX, mouseY);
+
+        provider = new DataProvider();
+        provider.getCrematoriaList(this);
         
-        crematorias = new DataProvider().getCrematoriaList();
+        canvas = this;
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                provider.getComplaintList(canvas);
+            }
+        });
+        
+        
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    //Give first thread a little head start to load some data in the complaint list
+                    //This will prevent the list to be empty when this thread starts
+                    Thread.sleep(10);
+                    provider.getDataWithCoordinates(canvas);
+                } catch (Exception ex) {
+                    Logger.getLogger(Canvas.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
+        t1.setPriority(Thread.MIN_PRIORITY);
+        t1.start();
+        t2.setPriority(Thread.MIN_PRIORITY);
+        t2.start();
     }
 
     @Override
     public void draw() {
+        System.out.println(complaints.size() + " - " + complaintsCoordinates.size());
         clear();
         mousePosition = new Vector2<>(mouseX, mouseY);
 
@@ -67,15 +106,15 @@ public class Canvas extends PApplet {
         pushMatrix();
         translate(-position.getX(), -position.getY());
         scale(scale);
-        
+
         image(testImage, 0, 0);
-        
-        for (Crematoria crematoria: crematorias) {
+
+        for (Crematoria crematoria : crematorias) {
             fill(255, 0, 0);
             stroke(0);
             ellipse(map(crematoria.getRdX(), 56682, 107674, drawArea.getX(), drawArea.getX() + drawArea.getWidth()), map(crematoria.getRdY(), 447962, 431038, drawArea.getY(), drawArea.getY() + drawArea.getHeight()), 5, 5);
         }
-        
+
         // Static / Absolute, for UI
         popMatrix();
         fill(0);
@@ -93,7 +132,7 @@ public class Canvas extends PApplet {
     public void mouseDragged() {
         Vector2<Float> mousePositionDelta = new Vector2<>((float) mousePosition.getX() - mouseX, (float) mousePosition.getY() - mouseY);
         position = Vector2.Sum(position, Vector2.Divide(mousePositionDelta, 5));
-        
+
         ClampPosition();
     }
 
@@ -105,24 +144,29 @@ public class Canvas extends PApplet {
     @Override
     public void mouseWheel(MouseEvent event) {
         float e = -event.getCount();
-        
+
         // Restrict scale so it won't show an empty screen when the user zooms too far out
-        if(!(testImage.width * (scale + e / 10) < viewport.getX() || testImage.height * (scale + e / 10) < viewport.getY()) && scale + e / 10 < maxScale)
+        if (!(testImage.width * (scale + e / 10) < viewport.getX() || testImage.height * (scale + e / 10) < viewport.getY()) && scale + e / 10 < maxScale) {
             scale += e / 10;
+        }
 
         ClampPosition();
     }
-    
+
     //To-do: Should be a method with parameters to clamp, so that it can be used for both position and scale
     private void ClampPosition() {
         // Clamp position so it won't show an empty screen when the user moves too far
-        if(position.getX() < 0)
+        if (position.getX() < 0) {
             position.setX(0f);
-        if(position.getX() > worldSize.getX() * scale - viewport.getX())
+        }
+        if (position.getX() > worldSize.getX() * scale - viewport.getX()) {
             position.setX(worldSize.getX() * scale - viewport.getX());
-        if(position.getY() < 0)
+        }
+        if (position.getY() < 0) {
             position.setY(0f);
-        if(position.getY() > worldSize.getY() * scale - viewport.getY())
+        }
+        if (position.getY() > worldSize.getY() * scale - viewport.getY()) {
             position.setY(worldSize.getY() * scale - viewport.getY());
+        }
     }
 }
--- Dev8-Stench/src/main/java/com/swenandjesse/dev8/stench/data/DataProvider.java
+++ Dev8-Stench/src/main/java/com/swenandjesse/dev8/stench/data/DataProvider.java
@@ -6,6 +6,7 @@
 package com.swenandjesse.dev8.stench.data;
 
 import com.sun.javafx.fxml.builder.URLBuilder;
+import com.swenandjesse.dev8.stench.Canvas;
 import com.swenandjesse.dev8.stench.HelperMethods;
 import com.swenandjesse.dev8.stench.models.Complaint;
 import com.swenandjesse.dev8.stench.models.ComplaintCoordinates;
@@ -24,143 +25,119 @@ import java.util.Scanner;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URI;
+import java.util.Collections;
 import processing.data.JSONArray;
 import processing.data.JSONObject;
-
 
 /**
  *
  * @author Jesse
  */
 public class DataProvider {
-    
-    //Used for old lookup method for postcode coordinates, with the use of the google api, this is deprecated
+
+    //Used for old lookup method for postcode coordinates, with the use of the google api, this one is deprecated
     //private final String API_KEY = "7aKDCk1a2C12QMLRq7coN23h4Fcu4Lar5KaTLZVJ";
     
-    public String getData(URL url) {
-        String allData = "";
+    private final String API_KEY = "AIzaSyC-c1Hy0PuEoG7WpFLHQAXdOI1bqPJRziM";
+
+    public void getComplaintList(Canvas canvas) {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("HACKATON--KLACHTEN-2011.csv").getFile());
         try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+            BufferedReader br = new BufferedReader(new InputStreamReader(file.toURI().toURL().openStream()));
             Iterator lines = br.lines().iterator();
             //Skip table headers
             lines.next();
-            int counter = 0;
             while (lines.hasNext()) {
                 String line = (String) lines.next();
-                allData += line + "\n";
-                counter++;
-                System.out.println(counter);
+                Scanner lineScanner = new Scanner(line);
+                lineScanner.useDelimiter(";");
+                Complaint c = new Complaint();
+                c.setComplaintMedium(lineScanner.next());
+                c.setDate(lineScanner.next());
+                c.setCount(Integer.parseInt(lineScanner.next()));
+                c.setStreetName(lineScanner.next());
+                c.setPostCode(lineScanner.next());
+                c.setCity(lineScanner.next());
+                c.setComplaintType(lineScanner.next());
+                c.setComplaintSubType(lineScanner.next());
+                c.setComplaintSubSubType(lineScanner.next());
+                c.setFeedbackRequested(lineScanner.next().equalsIgnoreCase("j"));
+                
+                canvas.complaints.add(c);
             }
         } catch (IOException e) {
             System.err.println("IO Exception occured while reading the data!");
         }
-        return allData;
     }
-    
-    public List<Complaint> getDataList() {
-        List<Complaint> complaints = new ArrayList<>();
-        String data = "";
+
+    public void getDataWithCoordinates(Canvas canvas) {
         try {
-            ClassLoader classLoader = getClass().getClassLoader();
-            File file = new File(classLoader.getResource("HACKATON--KLACHTEN-2011.csv").getFile());
-            data = getData(file.toURI().toURL());
-        } catch (IOException e) {
-            System.err.println("IO Exception occured while transforming data to list!");
-        }
-        
-        Scanner fileScanner = new Scanner(data);
-        fileScanner.useDelimiter("\n");
-        
-        while (fileScanner.hasNext()){
-            String line = fileScanner.next();
-            Scanner lineScanner = new Scanner(line);
-            lineScanner.useDelimiter(";");
-            Complaint c = new Complaint();
-            c.setComplaintMedium(lineScanner.next());
-            c.setDate(lineScanner.next());
-            c.setCount(Integer.parseInt(lineScanner.next()));
-            c.setStreetName(lineScanner.next());
-            c.setPostCode(lineScanner.next());
-            c.setCity(lineScanner.next());
-            c.setComplaintType(lineScanner.next());
-            c.setComplaintSubType(lineScanner.next());
-            c.setComplaintSubSubType(lineScanner.next());
-            c.setFeedbackRequested(lineScanner.next().equalsIgnoreCase("j"));
-            
-            if(c.getComplaintType().equalsIgnoreCase("Stank")){
-                complaints.add(c);
-            }
-        }
-        
-        return complaints;
-    }
-    
-    public List<ComplaintCoordinates> getDataWithCoordinates() {
-        List<ComplaintCoordinates> complaintsCoordinatesList = new ArrayList<>();
-        try {
-            List<Complaint> complaints = getDataList();
             StringBuilder sb = new StringBuilder();
-            String baseURL = "http://maps.googleapis.com/maps/api/geocode/json?address=";
+            String baseURL = "http://maps.googleapis.com/maps/api/geocode/json?key=";
             sb.append(baseURL);
-            for (Complaint c : complaints){
-                if (!c.getPostCode().equals("")){
+            sb.append(API_KEY);
+            for (int i = 0; i < canvas.complaints.size(); i++) {
+                Complaint c = canvas.complaints.get(i);
+                if (!c.getPostCode().equals("") && c.getComplaintType().equalsIgnoreCase("Stank")) {
                     sb.setLength(baseURL.length());
+                    sb.append("&address=");
                     sb.append(c.getPostCode());
-                    System.out.println(sb.toString());
+                    //System.out.println(sb.toString());
                     JSONObject jObject = getResponseJSON(new URL(sb.toString()));
                     ComplaintCoordinates cc = new ComplaintCoordinates();
                     cc.setComplaint(c);
                     cc.setCoordinates(getCoordinates(jObject));
-                    complaintsCoordinatesList.add(cc);
+                    canvas.complaintsCoordinates.add(cc);
                 }
             }
-        } catch (MalformedURLException e){
+        } catch (MalformedURLException e) {
             System.out.println("Exception occurred during converting");
         }
-        return complaintsCoordinatesList;
+
     }
-    
-    private JSONObject getResponseJSON(URL url){
-        String json  = "";
-        try {            
+
+    private JSONObject getResponseJSON(URL url) {
+        String json = "";
+        try {
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
             int responseCode = conn.getResponseCode();
-            if (responseCode == HttpURLConnection.HTTP_OK){
+            if (responseCode == HttpURLConnection.HTTP_OK) {
                 BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
                 String line;
-                while ((line = br.readLine()) != null){
+                while ((line = br.readLine()) != null) {
                     json += line + "\n";
                 }
                 br.close();
             }
-            
-        } catch (Exception e){
+
+        } catch (Exception e) {
             System.out.println("Http request failed!");
         }
         //System.out.println(json);
         JSONObject jObject = JSONObject.parse(json);
-        
+
         return jObject;
     }
-    
-    private Vector2 getCoordinates(JSONObject jObject){      
-        float latitude = 0, longitude = 0;
-        
+
+    private Vector2 getCoordinates(JSONObject jObject) {
+        double latitude = 0, longitude = 0;
+
         JSONArray results = jObject.getJSONArray("results");
-        if (results.size() > 0){
+        if (results.size() > 0) {
             JSONObject allResults = results.getJSONObject(0);
             JSONObject geometry = allResults.getJSONObject("geometry");
             JSONObject location = geometry.getJSONObject("location");
-            latitude = location.getFloat("lat");
-            longitude = location.getFloat("lng");
+            latitude = location.getDouble("lat");
+            longitude = location.getDouble("lng");
         }
-        
+
         Vector2 decimalCoordinates = new Vector2(latitude, longitude);
-        
+
         return HelperMethods.convertFromDecimalToRd(decimalCoordinates);
     }
-    
+
 //    public String getData(URL url) {
 //        String allData = "";
 //        try {
@@ -175,37 +152,31 @@ public class DataProvider {
 //        }
 //        return allData;
 //    }
-
-    public List<Crematoria> getCrematoriaList() {
-        List<Crematoria> crematoriaList = new ArrayList<>();
+    public void getCrematoriaList(Canvas canvas) {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("Crematoria_Rotterdam_eo.csv").getFile());
         String data = "";
         try {
-            ClassLoader classLoader = getClass().getClassLoader();
-            File file = new File(classLoader.getResource("Crematoria_Rotterdam_eo.csv").getFile());
-            data = getData(file.toURI().toURL());
+            BufferedReader br = new BufferedReader(new InputStreamReader(file.toURI().toURL().openStream()));
+            Iterator lines = br.lines().iterator();
+            //Skip table headers
+            lines.next();
+            
+            while (lines.hasNext()) {
+                String line = (String) lines.next();
+                Scanner lineScanner = new Scanner(line);
+                lineScanner.useDelimiter(";");
+                Crematoria crematoria = new Crematoria();
+                crematoria.setName(lineScanner.next());
+                crematoria.setAddress(lineScanner.next());
+                crematoria.setCity(lineScanner.next());
+                crematoria.setRdX(Integer.parseInt(lineScanner.next()));
+                crematoria.setRdY(Integer.parseInt(lineScanner.next()));
+                canvas.crematorias.add(crematoria);
+            }
+            
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        Scanner fileScanner = new Scanner(data);
-        fileScanner.useDelimiter("\n");
-
-        // Skip first row with column names
-        fileScanner.next();
-
-        while (fileScanner.hasNext()) {
-            String line = fileScanner.next();
-            Scanner lineScanner = new Scanner(line);
-            lineScanner.useDelimiter(";");
-            Crematoria crematoria = new Crematoria();
-            crematoria.setName(lineScanner.next());
-            crematoria.setAddress(lineScanner.next());
-            crematoria.setCity(lineScanner.next());
-            crematoria.setRdX(Integer.parseInt(lineScanner.next()));
-            crematoria.setRdY(Integer.parseInt(lineScanner.next()));
-            crematoriaList.add(crematoria);
-        }
-
-        return crematoriaList;
     }
 }
